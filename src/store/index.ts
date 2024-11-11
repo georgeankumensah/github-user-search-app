692d29132de14d015d5e2217ad5257c702a83571
GEORGE ANKU-MENSAH <92539173+georgeankumensah@users.noreply.github.com>
2024-11-11 17:07:09 +0000

    fix: zustand store

---
+++ src/store/index.ts
@@ -0,0 +1,5 @@
+import useUserStore from "./useUserStore";
+
+
+
+export default {useUserStore}
--- src/store/useUserStore.ts
+++ src/store/useUserStore.ts
@@ -1,2 +1,28 @@
 import { create } from "zustand";
 
+const useUserStore = create((set) => ({
+  user: null,
+  loading: false,
+  error: false,
+  fetchUser: async (username: string) => {
+    set({ loading: true, error: null });
+    try {
+      const response = await fetch(
+        `https://api.github.com/users/${username}`
+      ).then((res) => res.json());
+      if (!response.ok) {
+        if (response.status === 404) {
+          throw new Error("No results");
+        }
+        throw new Error(`request error! Status: ${response.status}`);
+      }
+
+      set({ loading: false, error: null, user: response.data });
+      console.log("user", response.data);
+    } catch (error) {
+      set({ error: (error as Error).message, loading: false });
+    }
+  },
+}));
+
+export default useUserStore;
